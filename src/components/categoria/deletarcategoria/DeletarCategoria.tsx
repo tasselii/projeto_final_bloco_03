e2c77357f043d31f412a718947146cb0c3790826
Thiago Tasseli <thiagotasseli@outlook.com.br>
2025-07-02 12:39:51 -0300

    :wrench: correção para o deploy

--- src/components/categoria/deletarcategoria/DeletarCategoria.tsx
+++ src/components/categoria/deletarcategoria/DeletarCategoria.tsx
@@ -5,19 +5,17 @@ import { deletar, listar } from "../../../services/Service"
 import type Categoria from "../../../models/Categoria"
 
 function DeletarCategoria() {
-
     const navigate = useNavigate()
-
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
 
-    const { id } = useParams<{ id: string }>();
+    const { id } = useParams<{ id: string }>()
 
     async function buscarPorId(id: string) {
         try {
             await listar(`/categorias/${id}`, setCategoria)
         } catch (error: any) {
-            alert("Tema não encontrado!")
+            alert("Categoria não encontrada!")
         }
     }
 
@@ -32,9 +30,7 @@ function DeletarCategoria() {
 
         try {
             await deletar(`/categorias/${id}`)
-
             alert("Categoria apagada com sucesso")
-
         } catch (error) {
             alert("Erro ao apagar a categoria")
         }
@@ -51,13 +47,14 @@ function DeletarCategoria() {
         <div className="container w-1/3 mx-auto">
             <h1 className="py-4 text-4xl text-center">Deletar Categoria</h1>
             <p className="mb-4 font-semibold text-center">
-                Você tem certeza de que deseja apagar a categoria a seguir?</p>
+                Você tem certeza de que deseja apagar a categoria a seguir?
+            </p>
+
             <div className="flex flex-col justify-between overflow-hidden border rounded-2xl">
-                <header
-                    className="px-6 py-2 text-2xl font-bold text-white bg-slate-600">
+                <header className="px-6 py-2 text-2xl font-bold text-white bg-slate-600">
                     Categoria
                 </header>
-                <p className="h-full p-8 text-3xl bg-white">{}</p>
+
                 <div className="flex">
                     <button
                         className="w-full py-2 bg-red-500 text-slate-50 hover:bg-red-600"
@@ -65,24 +62,27 @@ function DeletarCategoria() {
                     >
                         Não
                     </button>
+
                     <button
                         className="flex items-center justify-center w-full bg-teal-600 text-slate-50 hover:bg-teal-800"
                         onClick={deletarCategoria}
                     >
-                        {isLoading ?
+                        {isLoading ? (
                             <RotatingLines
                                 strokeColor="white"
                                 strokeWidth="5"
                                 animationDuration="0.75"
                                 width="24"
                                 visible={true}
-                            /> :
+                            />
+                        ) : (
                             <span>Sim</span>
-                        }
+                        )}
                     </button>
                 </div>
             </div>
         </div>
     )
 }
+
 export default DeletarCategoria
