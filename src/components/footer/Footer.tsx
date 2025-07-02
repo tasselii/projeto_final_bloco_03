87a05303d866fe15d6a3bacdc1e09b8f1ccc3078
Thiago Tasseli <thiagotasseli@outlook.com.br>
2025-07-02 12:03:00 -0300

    :wrench: correção para o deploy

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react"
+import { GithubLogo, LinkedinLogo } from "@phosphor-icons/react"
 
 function Footer() {
 
